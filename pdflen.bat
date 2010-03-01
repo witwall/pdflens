1ea0daa898e7a2480b93474c551cda0c95b3bab8
nisen.cn <nisen.cn@85490e82-24ff-11df-97fb-d110b628d502>
2010-03-01 07:31:35 +0000

    move from pdfboxgui

---
+++ pdflen.bat
@@ -0,0 +1,6 @@
+set cp=./bin;
+set cp=%CP%;external/commons-logging-1.1.1.jar
+set cp=%CP%;external/pdfbox-1.0-SNAPSHOT.jar
+set cp=%CP%;external/fontbox-1.0.0.jar
+set cp=%CP%;external/jempbox-1.0.0.jar
+java -cp %cp% org.apache.pdflens.Main
